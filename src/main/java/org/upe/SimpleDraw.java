0bbe2b4e38570bbd54a5e493c6443efcd8275f09
Victor Salgado <joaosalgado605@gmail.com>
2024-08-28 18:11:28 -0300

    fist commit

---
+++ src/main/java/org/upe/SimpleDraw.java
@@ -0,0 +1,13 @@
+package org.upe;
+
+/**
+ * Hello world!
+ *
+ */
+public class SimpleDraw
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
